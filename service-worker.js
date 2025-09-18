31784416d95e5de81f88f70402797271f79652ef
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2025-09-17 21:14:58 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "sprite-editor-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./editor.js",
+  "./manifest.json",
+  "./icons/icon-192.png"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+  );
+});
